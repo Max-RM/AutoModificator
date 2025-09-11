5639eb8b9ed07b06b4e6e6f64eb246a048f17747
mdlcgroup <tnt.entertainment.32768@gmail.com>
2025-09-11 17:21:06 +0500

    Update Build_All.bat

--- Build_All.bat
+++ Build_All.bat
@@ -1,7 +1,7 @@
 @echo off
 call build_llvm_x64.bat
 call build_llvm_x86.bat
-call build_llvm_arm64.bat
-call build_llvm_arm.bat
+call build_llvm_ARM64.bat
+call build_llvm_ARM32.bat
 echo All builds finished.
 pause
